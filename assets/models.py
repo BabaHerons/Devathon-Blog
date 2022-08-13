7b834af5c8adc01ed7860f9ffc4c432681178b4b
Manmay <manmay.singh@gmail.com>
2022-08-13 17:38:46 +0530

    deleted the pycache foler

--- assets/models.py
+++ assets/models.py
@@ -2,7 +2,27 @@ from assets import db
 
 
 class User(db.Model):
-    pass
+    id = db.Column(db.Integer(), primary_key = True)
+    username = db.Column(db.String(11), unique = True, nullable = False)
+    password = db.Column(db.String(), nullable = False)
+    email = db.Column(db.String(30), nullable=False)
+    fullname = db.Column(db.String(25), nullable=False)
+    dp = db.Column(db.LargeBinary, nullable=True)
+    mob = db.Column(db.String(10), nullable=True)
 
-class UserProfile(db.Model):
-    pass
+class Posts(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    post = db.Column(db.String(5000), nullable = False, unique = True)
+    likes_count = db.Column(db.Integer())
+    dislikes_count = db.Column(db.Integer())
+    userid = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+class Comments(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    comment = db.Column(db.String(1000), nullable = False)
+    userid = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    postid = db.Column(db.Integer(), db.ForeignKey('posts.id'))
+
+class LikedPosts(db.Model):
+    userid = db.Column(db.Integer(), db.ForeignKey('user.id'), primary_key = True)
+    postid = db.Column(db.Integer(), db.ForeignKey('posts.id'), primary_key = True)
--- assets/routes.py
+++ assets/routes.py
@@ -1,5 +1,37 @@
-from assets import app
+from flask import render_template, redirect, url_for, request
+from assets import app, db
+from assets.models import User, Posts, Comments, LikedPosts
+
 
 @app.route('/')
 def home():
-    return 'Hey I am working.'
+    return render_template('home.html')         # here need to send signedIn = true or false as a string value.
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return render_template('home.html', signedIn = 'true', user = user)
+    return render_template('signuporlogin.html')
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        fullname = request.form.get('fullname')
+        dp = request.files['dp']
+        mob = request.form.get('mob')
+        user = User.query.filter_by(username=username, password=password).first()
+        if not user:
+            user = User(username=username, password=password, email=email, fullname=fullname, dp=dp.read(), mob=mob)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('home'))
+    return render_template('signuporlogin.html', signup = 'true')
+
+
