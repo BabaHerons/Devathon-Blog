32fac4498a3c32a5b751e773190455279e9ea3fd
Manmay <manmay.singh@gmail.com>
2022-08-13 17:13:27 +0530

    Following changes have been made:
        1. updated the models.py:
            a. added all of the tables information user, comments, posts, likedposts
    
        2. updated the routes.py:
            a. updated the login routes, signup routes
            b. get the value from the user and fed it into the database
    
        3. updated the html files:
            a. added the if else statement for the navbar home, login or signup
            b. in signuporlogin html file use the if else statemente to keep differences
               between the login form and the signup form
            c. same like (b.) added the changes in the home.html file for showing the name
               of the user if the user has signed in.

--- assets/routes.py
+++ assets/routes.py
@@ -1,17 +1,37 @@
 from flask import render_template, redirect, url_for, request
 from assets import app, db
+from assets.models import User, Posts, Comments, LikedPosts
 
 
 @app.route('/')
 def home():
     return render_template('home.html')         # here need to send signedIn = true or false as a string value.
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return render_template('home.html', signedIn = 'true', user = user)
     return render_template('signuporlogin.html')
 
 @app.route('/signup', methods = ['GET','POST'])
 def signup():
-    return render_template('signuporlogin.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        fullname = request.form.get('fullname')
+        dp = request.files['dp']
+        mob = request.form.get('mob')
+        user = User.query.filter_by(username=username, password=password).first()
+        if not user:
+            user = User(username=username, password=password, email=email, fullname=fullname, dp=dp.read(), mob=mob)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('home'))
+    return render_template('signuporlogin.html', signup = 'true')
 
 
