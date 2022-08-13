230e5e55e65961be9767990305a63a86bad1509f
Manmay <manmay.singh@gmail.com>
2022-08-13 13:46:03 +0530

    added the signuporlogin.html and related routes

--- assets/routes.py
+++ assets/routes.py
@@ -1,16 +1,17 @@
 from flask import render_template, redirect, url_for, request
 from assets import app, db
 
+
 @app.route('/')
 def home():
     return render_template('home.html')         # here need to send signedIn = true or false as a string value.
 
-
-@app.route('/signup')
-def signup():
-    return 'signup page is working'
-
 @app.route('/login')
 def login():
-    return 'login page is working'
+    return render_template('signuporlogin.html')
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    return render_template('signuporlogin.html')
+
 
